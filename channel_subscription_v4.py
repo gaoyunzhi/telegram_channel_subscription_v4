b7e6af937d7ce90a338c87906166288bad899539
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-12-09 15:23:01 -0800

    commit

--- channel_subscription_v4.py
+++ channel_subscription_v4.py
@@ -5,51 +5,10 @@ INTERVAL = 60 * 24 * 60
 
 from telegram.ext import Updater, MessageHandler, Filters
 
-import export_to_telegraph
-from html_telegraph_poster import TelegraphPoster
 from telegram_util import splitCommand, log_on_fail, autoDestroy, formatChat
-from db import Subscription, Sent, Pool
+from db import Source
 import loop
 from .common import tele, debug_group
-
-
-def msgTelegraphToken(msg):
-	user_id = msg.from_user.id
-	if user_id in TELEGRAPH_TOKENS:
-		p = TelegraphPoster(access_token = TELEGRAPH_TOKENS[user_id])
-	else:
-		p = TelegraphPoster()
-		r = p.create_api_token(msg.from_user.first_name, msg.from_user.username)
-		TELEGRAPH_TOKENS[user_id] = r['access_token']
-		saveTelegraphTokens()
-	msgAuthUrl(msg, p)
-
-def msgAuthUrl(msg, p):
-	r = p.get_account_info(fields=['auth_url'])
-	msg.reply_text('Use this url to login in 5 minutes: ' + r['auth_url'])
-
-def getTelegraph(msg, url):
-	user_id = msg.from_user.id
-	if user_id not in TELEGRAPH_TOKENS:
-		msgTelegraphToken(msg)
-	export_to_telegraph.token = TELEGRAPH_TOKENS[user_id]
-	return export_to_telegraph.export(url, True, force = True) # DEBUG, remove second param when go prod
-
-@log_on_fail(debug_group)
-def export(update, context):
-	msg = update.message
-	for item in msg.entities:
-		if (item["type"] == "url"):
-			url = msg.text[item["offset"]:][:item["length"]]
-			if not '://' in url:
-				url = "https://" + url
-			u = getTelegraph(msg, url)
-			msg.reply_text(u)
-			if msg.from_user.id not in known_users:
-				r = debug_group.send_message( 
-					text=getDisplayUser(msg.from_user) + ': ' + u, 
-					parse_mode='Markdown')
-
 
 def commandImp(msg):
 	autoDestroy(msg)
@@ -60,9 +19,9 @@ def commandImp(msg):
 		# subscription mode
 		pass # TODO
 	elif command == "s4_source_add":
-		sub_db.add(tele.bot.getChat(text).id)
+		Source.add(tele.bot.getChat(text).id)
 	elif command == "s4_source_delete":
-		sub_db.remove(tele.bot.getChat(text).id)
+		Source.remove(tele.bot.getChat(text).id)
 	elif command == "s4_source_list":
 		pass # intentional
 	else:
