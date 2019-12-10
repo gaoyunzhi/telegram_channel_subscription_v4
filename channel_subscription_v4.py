88edda28890cd059f15260eeaf0101f3ed982ac9
Yunzhi Gao <gaoyunzhi@gmail.com>
2019-12-09 17:29:49 -0800

    fix

--- channel_subscription_v4.py
+++ channel_subscription_v4.py
@@ -5,28 +5,32 @@ INTERVAL = 60 * 24 * 60
 
 from telegram.ext import Updater, MessageHandler, Filters
 
-from telegram_util import splitCommand, log_on_fail, autoDestroy, formatChat
+from telegram_util import splitCommand, log_on_fail, autoDestroy, formatChat, matchKey
 from db import Source, Subscription
 import loop
 from common import tele, debug_group
 from iterateMessage import iterateMessage
+import traceback as tb
 
 def commandImp(msg):
 	autoDestroy(msg)
 	command, text = splitCommand(msg.text)
-	if command == "s4_get":
+	print(command, text)
+	if "s4_g" in command:
 		return msg.reply_text(iterateMessage(msg.chat.id), quote=False)
-	elif command == "s4_subscribe":
-		Subscription.add(msg.chat.id, text)
-	elif command == "s4_source_add":
+	elif matchKey(command, ["s4_source_add", "s4_sa", "s4_a"]):
 		autoDestroy(msg.reply_text(Source.add(tele.bot.getChat(text).id)))
-	elif command == "s4_source_delete":
+	elif matchKey(command, ["s4_source_delete", "s4_sd", "s4_d"]):
 		autoDestroy(msg.reply_text(Source.remove(tele.bot.getChat(text).id)))
-	elif command == "s4_source_list":
+	elif matchKey(command, ["s4_source_list", "s4_sl", "s4_l"]):
 		pass # intentional
+	elif matchKey(command, ["s4_sub", "s4_s"]):
+		Subscription.add(msg.chat.id, text)
 	else:
 		return
-	if command.startswith(s4_source):
+	print('here1.2')
+	if matchKey(command, ["s4_source", "s4_sl", "s4_l", "s4_sa", "s4_a", "s4_sd", "s3_d"]):
+		print('here2')
 		sources = [str(index) + ': ' + formatChat(tele.bot, chat_id) for \
             index, chat_id in enumerate(subscriptions)]
 		autoDestroy(msg.reply_text('Source list: \n\n' + '\n'.join(sources), 
@@ -36,14 +40,18 @@ def commandImp(msg):
 		autoDestroy(msg.reply_text("success"))
 
 def command(update, context):
+	print('here')
 	msg = update.effective_message
 	try:
 		commandImp(msg)
 	except Exception as e:
+		print('here3')
 		autoDestroy(msg.reply_text(str(e)))
+		tb.print_exc()
 		raise e
+	print('here4')
 
-tele.dispatcher.add_handler(MessageHandler(Filters.update.channel_posts & Filters.command, command))
+tele.dispatcher.add_handler(MessageHandler(Filters.command, command))
 
 tele.start_polling()
 tele.idle()
